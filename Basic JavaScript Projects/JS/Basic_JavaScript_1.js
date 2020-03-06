a1e32150dc8adb59eee9cf4b6a19b27947c64980
jaresj <60714611+jaresj@users.noreply.github.com>
2020-03-06 11:45:08 -0800

    3/6 JavaScript

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,20 @@
+
+var B = "Hello, World!";
+window.alert(B);
+
+var A = "Good morning Justice! Remember,"
+ +"\"Don't be afraid to be great.\"";
+ var A = A.fontcolor("blue")
+document.write(A); 
+
+var C = "<br> My favorite movie is:"
++ " Lord of the Rings<br>";
+var C = C.fontcolor("red")
+document.write(C);
+
+var Family = "The Jares's<br>", Dad = "Dean<br>", Mom = "Vickie<br>",
+Daughter = "Tiahna<br>", Son = "Justice<br>";
+
+document.write(Daughter);
+
+document.write(5 + 6);
